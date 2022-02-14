495e5bd34c0f85101226342a3190bfd5617f6e44
Heroin-lab <vip.serkin@gmail.com>
2022-02-14 12:04:06 +0200

    add server response mok

--- JS_second_hw/script.js
+++ JS_second_hw/script.js
@@ -1,3 +1,33 @@
+let serverResp = [
+    {
+        "Id": "4",
+        "Product_name": "Original burger",
+        "Prod_type_name": "burger",
+        "Price": 3,
+        "Img": "https://316024.selcdn.ru/wiget/4d4c871a-107f-11e7-80df-d8d38565926f/ba3a1ca2-2ec6-4cf4-a843-f7f0fc0f175a_Medium_.jpg",
+        "Supplier": "Burger Club",
+        "Quantity": 0
+    },
+    {
+        "Id": "5",
+        "Product_name": "Crispy Chicken burger",
+        "Prod_type_name": "burger",
+        "Price": 4,
+        "Img": "https://316024.selcdn.ru/wiget/4d4c871a-107f-11e7-80df-d8d38565926f/93bce037-709e-41a0-9beb-ab3670663b40_Medium_.jpg",
+        "Supplier": "Burger Club",
+        "Quantity": 0
+    },
+    {
+        "Id": "6",
+        "Product_name": "Bacon Cheese Burger",
+        "Prod_type_name": "burger",
+        "Price": 7,
+        "Img": "https://316024.selcdn.ru/wiget/4d4c871a-107f-11e7-80df-d8d38565926f/49e75c35-6fff-4a0f-8d6a-959cf4721c74_Medium_.jpg",
+        "Supplier": "Burger Club",
+        "Quantity": 0
+    }
+]
+
 let productType = {
     category_name:"burger",
     permissions: [
@@ -23,8 +53,6 @@ async function getAllProducts(data) {
 function htmlConstructor(obj) {
     let element = document.getElementById("elements")
 
-    console.log(obj)
-
     for (let i = 0; i < obj.length; i++) {
         element.innerHTML +=
             `<div id="element">
@@ -38,7 +66,6 @@ function htmlConstructor(obj) {
 }
 
 window.onload = getAllProducts(productType)
-
 
 // 2
 const copy = obj => {
